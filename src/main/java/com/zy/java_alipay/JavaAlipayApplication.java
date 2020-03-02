e60666dce00682052c54c603a28f13c99c1624c7
zhangyao <zy963613606@aliyun.com>
2020-03-02 10:32:18 +0800

    init

---
+++ src/main/java/com/zy/java_alipay/JavaAlipayApplication.java
@@ -0,0 +1,13 @@
+package com.zy.java_alipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAlipayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAlipayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zy/java_alipay/JavaAlipayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zy.java_alipay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAlipayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
