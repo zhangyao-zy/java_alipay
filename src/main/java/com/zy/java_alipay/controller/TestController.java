97bc640930c37c7d6b6e4218e52c773ab6c15d01
zhangyao <zy963613606@aliyun.com>
2020-03-19 16:39:31 +0800

    refactor(测试jenkins): 测试jenkins

--- src/main/java/com/zy/java_alipay/controller/TestController.java
+++ src/main/java/com/zy/java_alipay/controller/TestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     @GetMapping("/test")
-    public String test(@PathVariable String message){
+    public String test(String message){
         return message;
 
     }
