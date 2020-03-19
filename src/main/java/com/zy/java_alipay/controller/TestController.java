a71aacc039e3832546b698fbc043888e21ff6865
zhangyao <zy963613606@aliyun.com>
2020-03-19 15:13:28 +0800

    refactor(测试jenkins): 测试jenkins

---
+++ src/main/java/com/zy/java_alipay/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.zy.java_alipay.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: zhangyao
+ * @create:2020-03-19 14:51
+ **/
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test(@PathVariable String message){
+        return message;
+    }
+}
